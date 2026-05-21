5b574a4259bbe3c8f6c9e11aa5494d4011202277
sam <22.samarpan@gmail.com>
2026-05-21 19:28:51 +0530

    new about page added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import About from './components/ui/About.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
       <Route path="/:username" element={<App />} />
     </Routes>
   </BrowserRouter>
